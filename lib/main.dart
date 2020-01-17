d4240d916b49d06ed9f6d1b0a3e9f98d01366621
TheMuellenator <themuellenator@users.noreply.github.com>
2020-01-17 18:59:08 +0000

    Updating sdk versions.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          body: MagicBall(),
+        ),
       ),
     );
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({Key? key}) : super(key: key);
+
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballRandomNumber = 2;
+
+  void ballRandom() {
+    setState(() {
+      ballRandomNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextButton(
+            onPressed: () {
+              ballRandom();
+            },
+            child: Image.asset('images/ball$ballRandomNumber.png')),
+      ),
+    );
+  }
+}
